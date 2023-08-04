71f385efb1237c8e6882eb102826d4764d9a8e2a
BrandonJosue03 <alexander.abo52@gmail.com>
2023-08-03 21:04:02 -0600

    añadido

---
+++ Semana 8/proyectoJS/js/menu.js
@@ -0,0 +1,26 @@
+ menuprincipal.innerHTML += ` 
+ 
+ <ul class="nav nav-tabs" id="navId" role="tablist">
+ <li class="nav-item">
+     <a href="index.html" class="nav-link" data-bs-toggle="tab" aria-current="page">Inicio</a>
+ </li>
+ <li class="nav-item">
+     <a href="https://paginas-web-cr.com/ApiPHP/" target"_blank" class="nav-link" data-bs-toggle="tab" aria-current="page">API</a>
+ </li>
+
+ <li class="nav-item dropdown">
+     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+     <div class="dropdown-menu">
+         <a class="dropdown-item" href="listarcurso.html">Listar Curso</a>
+         <a class="dropdown-item" href="">Another action</a>
+         <div class="dropdown-divider"></div>
+         <a class="dropdown-item" href="">Action</a>
+     </div>
+ </li>
+ <li class="nav-item" role="presentation">
+     <a href="#tab5Id" class="nav-link" data-bs-toggle="tab">Another link</a>
+ </li>
+ <li class="nav-item" role="presentation">
+     <a href="#" class="nav-link disabled" data-bs-toggle="tab">Disabled</a>
+ </li>
+</ul>`; 

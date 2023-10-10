ff87f3d07df7b9904dc199217e3a765489807969
BrandonJosue03 <alexander.abo52@gmail.com>
2023-10-09 20:02:17 -0600

    j

---
+++ Semana 18/test.js
@@ -0,0 +1,24 @@
+const titulo = "Semana 18";
+console.log(titulo);
+console.log(typeof titulo);
+
+const numerocualquiera = 10;
+console.log(numerocualquiera);
+console.log(typeof numerocualquiera);
+
+const bolean = true;
+console.log(bolean);
+console.log(typeof bolean);
+
+//Estaticos, maneja tipo de dato
+//c++
+//java
+//c#
+
+//por ejemplo 
+//number variable = 0;
+
+//dinamicos, no necesitan saber cual es el tipo de dato
+// js
+// python
+//var variable = 0;

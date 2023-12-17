81207605914ae6839582a5e494e848d014646ad3
BrandonJosue03 <alexander.abo52@gmail.com>
2023-12-17 13:15:03 -0600

    iii

--- proyectoReact/academia/src/componentePlantilla/menu.js
+++ proyectoReact/academia/src/componentePlantilla/menu.js
@@ -13,10 +13,10 @@ class Menu extends React.Component {
                     <a className="nav-link active" href="#" aria-current="page">Active link</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="ListarCurso">Link</a>
+                    <a className="nav-link" href="ListarCurso">Listar Curso</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="CrearCurso"> link</a>
+                    <a className="nav-link" href="CrearCurso"> Crear Curso</a>
                 </li>
             </ul>
 
